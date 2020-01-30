378f07d3bb782fd0bf647959170e8186c67384ca
Abdulaziz Ladan <abdulazizladan@gmail.com>
2020-01-30 04:44:41 +0100

    task: Begin creation of API and models

--- models/User.php
+++ models/User.php
@@ -1,10 +1,65 @@
 <?php
     class User{
+        
+        private $user_email;
+        
         public function __constructor(){
-
+            //
         }
 
-        public function login(){
+        public function login($email, $password){
+            //   
+            $stmt = "SELECT User.email FROM User WHERE User.email = ?? AND User.password = ?? LIMIT 1";
+            $this->email = $email;
+        }
+        
+        public function register($email, $password){
+            //
+            $stmt = "INSERT INTO `User`() VALUE() ";
+        }
+        
+        public function sendMessage($message_body, $recipient_id){
+            //
+            $stmt = "INSERT INTO TABLE";
+        }
+        
+        public function comment($post_id, $comment_body){
+            //
+            $stmt = "";
+        }
+        
+        public function react($post_id, $sentiment){
+            //
+            $stmt = "";
+        }
+        
+        public function logout(){
+            //
+            $stmt = "";
+        }
+        
+        public function changeName(){
             
+        }
+        
+        public function changePassword(){
+            
+        }
+        
+        public function changeLocation(){
+            
+        }
+        
+        public function follow($user_email){
+            
+        }
+        
+        public function changeProfilePicture(){
+            
+        }
+        
+        public function edit_profile(){
+            //
+            $stmt = "INSERT INTO User";
         }
     }
